45e4ef932b22125922db1c16800255b2c30cee45
Liangbai2333 <1739566851@qq.com>
2025-03-30 02:57:14 +0800

    pre for spawn detail.

--- template-src/layouts/SpawnDetail.tsx
+++ template-src/layouts/SpawnDetail.tsx
@@ -2,6 +2,15 @@ import PokeCard from "../components/PokeCard.tsx";
 import usePokemon from "../stores/usePokemon.ts";
 import {useParams} from "react-router";
 
+const CONTEXTS_MAPPING = {
+    grounded: '地面',
+    seafloor: '海底',
+    lavafloor: '岩浆底',
+    submerged: '水下',
+    surface: '水面',
+    fishing: '钓鱼'
+}
+
 export default function SpawnDetail() {
     const { index: i = "0" } = useParams<{name: string, index: string}>();
     const {pokemon, loading, error} = usePokemon()
@@ -12,15 +21,24 @@ export default function SpawnDetail() {
         return <div>No spawn details found</div>
     }
 
+    const detail = pokemon?.spawn_details?.[index];
+
     return (
         <div className="text-center font-mono">
             {loading && <p>Loading...</p>}
             {error && <p>{error}</p>}
-            {pokemon && (
+            {pokemon && detail && (
                 <PokeCard>
                     <PokeCard.Header pokemon={pokemon} />
 
-
+                    <div className="grow self-start justify-self-start mt-5 grid grid-cols-2">
+                        <div className="text-sm ml-4 text-left">
+                            <span className="text-gray-500">生成上下文</span>
+                            <div className="mt-2 px-1 text-gray-700 font-bold">
+                                {CONTEXTS_MAPPING[detail.context]}
+                            </div>
+                        </div>
+                    </div>
                 </PokeCard>
             )}
         </div>
