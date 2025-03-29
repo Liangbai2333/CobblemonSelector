17975a8295b22b0a99e4aad7c417fd4b4355930e
Liangbai2333 <1739566851@qq.com>
2025-03-29 17:32:52 +0800

    adjust page to ensure normal screenshot work

--- template-src/components/PokeCard.tsx
+++ template-src/components/PokeCard.tsx
@@ -2,8 +2,8 @@ import {ReactNode} from "react";
 
 export default function PokeCard({children}: { children: ReactNode[] }) {
     return (
-        <div className="flex h-screen w-screen justify-center">
-            <div className="pokemon-start shadow-xl rounded-xl flex-col max-h-screen max-w-screen items-center overflow-y-auto">
+        <div className="flex justify-center">
+            <div className="pokemon-start shadow-xl flex-col items-center">
                 {children}
             </div>
         </div>
